00fb192b0df332f3355655b9fe6745e6f3000f38
Carolina Ripe <scaror2@gmail.com>
2021-09-28 21:26:26 -0500

    ADD functions JS, Folders CSS and JS

---
+++ Sprint1/js/form2.js
@@ -0,0 +1,15 @@
+function validarCorreo(emailaddress){
+        
+    if (validateEmailAddress(emailaddress)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmailAddress(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+module.exports.validarCorreo = validarCorreo;
