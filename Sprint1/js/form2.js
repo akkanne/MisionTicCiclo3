7e61b7233da31d30f30f1e7f2cc132f923f531eb
Carolina Ripe <scaror2@gmail.com>
2021-09-30 22:01:54 -0500

    Settings file js

--- Sprint1/js/form2.js
+++ Sprint1/js/form2.js
@@ -1,15 +1,64 @@
-function validar_correo(emailaddress){
-        
-    if (validateEmailAddress(emailaddress)) {
-        return true;
+function validar_correo(email){
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    if (email.length >= 15 && email.length <=40){
+        if(email.indexOf('@') !== -1){
+            if((email.indexOf(".com") || email.indexOf(".org") || email.indexOf(".es") || email.indexOf(".co")) !== -1) {
+                if (re.test(email)) {
+                    //alert("Cumple la expresión");
+                    return true;
+                } else {
+                    //alert("No cumple la expresión");
+                    return false;
+                }
+            }else {
+                //alert("No hay .com, .org, .com .es");
+                return false;
+            }
+        } else {
+            //alert("No hay @");
+            return false;
+        }
     } else {
+        //alert("La cadena no aplica");
         return false;
     }
 }
 
-function validateEmailAddress(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+function validar_contrasena_usuario(usuario, contrasena ){
+    var okUsuario = validarPolitica(usuario)
+    //var okContrasena = validarPolitica(contrasena)
+    
+    var re = /(?=^[A-Z])/;
+    
+
+    if(okUsuario) //&& okContrasena)
+        return true;
+    else
+        return re.test(usuario)
+    
 }
 
+function validarPolitica(string){
+    var re = /[@]/;
+    
+    if (string.length >= 6){
+        //alert("la cadena es menor a 6");
+        return true;
+    }
+    if (string.length <= 12){
+        //alert("la cadena es mayor a 12");
+        return true;
+    }
+    if (re.test(string)){
+        //alert("la cadena CONTIENE UN @");
+        return true;    
+    }
+
+    return false;
+}
+
+
 module.exports.validar_correo = validar_correo;
+module.exports.validar_contrasena_usuario = validar_contrasena_usuario;
+module.exports.validarPolitica = validarPolitica;
